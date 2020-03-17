c463efa67489e19d04b2308d2e131291c11d8efe
EDUARDO BARBOSA <edu.bar.oliveira@gmail.com>
2020-03-17 20:37:21 +0000

    [fix] set correctly button on tabs component on reload page

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,91 +1,116 @@
-import React, { useState } from "react";
-import StepperComponent from "./Stepper";
-import { ButtonToggle, ButtonGroup } from "reactstrap";
-import "./Tabs.css";
-import { FaTags, FaBuffer, FaImage } from "react-icons/fa";
-import { FiScissors } from "react-icons/fi";
-import { IoIosFolderOpen } from "react-icons/io";
-import { MdChat } from "react-icons/md";
-import RouteContentArea from "./ContentArea";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import StepperComponent from './Stepper';
+import { ButtonToggle, ButtonGroup } from 'reactstrap';
+import './Tabs.css';
+import { FaTags, FaBuffer, FaImage } from 'react-icons/fa';
+import { FiScissors } from 'react-icons/fi';
+import { IoIosFolderOpen } from 'react-icons/io';
+import { MdChat } from 'react-icons/md';
+import RouteContentArea from './ContentArea';
+import { useHistory } from 'react-router-dom';
 
 function Tabs(props) {
   let [step, setStep] = useState(0);
   let history = useHistory();
   const [buttons, setButtons] = useState([
     {
-      title: "Metadados",
+      title: 'Metadados',
       icon: <FaTags className="icon-btn-tab" size={20} />,
       active: true,
-      path: "/"
+      path: '/'
     },
     {
-      title: "Edição",
+      title: 'Edição',
       icon: <FiScissors className="icon-btn-tab" size={20} />,
       active: false,
-      path: "/editor"
+      path: '/editor'
     },
     {
-      title: "Capítulos",
+      title: 'Capítulos',
       icon: <FaBuffer className="icon-btn-tab" size={20} />,
       active: false,
-      path: "/chapters"
+      path: '/chapters'
     },
     {
-      title: "Documentos",
+      title: 'Documentos',
       icon: <IoIosFolderOpen className="icon-btn-tab" size={20} />,
       active: false,
-      path: "/documents"
+      path: '/documents'
     },
     {
-      title: "Branding",
+      title: 'Branding',
       icon: <FaImage className="icon-btn-tab" size={20} />,
       active: false,
-      path: "/branding"
+      path: '/branding'
     },
     {
-      title: "Legendas",
+      title: 'Legendas',
       icon: <MdChat className="icon-btn-tab" size={20} />,
       active: false,
-      path: "/subtitles"
+      path: '/subtitles'
     }
   ]);
 
-  const onButtonClick = element => {
-    const index =
-      typeof element == "object"
-        ? buttons.findIndex(e => e.title === element.title)
-        : element;
-    const title = buttons[index].title;
-    setStep(index);
-    history.push(buttons[index].path);
-    setButtons(
-      buttons.map(b =>
-        b.title === title ? { ...b, active: true } : { ...b, active: false }
-      )
-    );
-  };
+  const updateActiveButton = useCallback(
+    path => {
+      let _path = path;
+      let index = buttons.findIndex(b => b.path === _path);
+      if (index === -1) {
+        index = 0;
+        _path = '/'; // update unexpected path for one that is expected
+      }
+      setStep(index);
+      setButtons(
+        buttons.map(b =>
+          b.path === _path ? { ...b, active: true } : { ...b, active: false }
+        )
+      );
+    },
+    [buttons]
+  );
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    updateActiveButton(path);
+  }, []); // must not put the dependencies here to avoid infinite loop
+
+  const onButtonClick = useCallback(
+    element => {
+      const index =
+        typeof element == 'object'
+          ? buttons.findIndex(e => e.title === element.title)
+          : element;
+      const path = buttons[index].path;
+      history.push(buttons[index].path);
+      updateActiveButton(path);
+    },
+    [buttons, history, updateActiveButton]
+  );
+
+  const renderTabButtons = useMemo(
+    () =>
+      buttons.map(element => {
+        return (
+          <ButtonToggle
+            key={element.title}
+            active={element.active}
+            className="btn-tab"
+            onClick={() => onButtonClick(element)}
+          >
+            {element.icon}
+            <span>{element.title}</span>
+          </ButtonToggle>
+        );
+      }),
+    [buttons, onButtonClick]
+  );
 
   return (
     <div className="container">
-      <ButtonGroup>
-        {buttons.map(element => {
-          return (
-            <ButtonToggle
-              key={element.title}
-              active={element.active}
-              className="btn-tab"
-              onClick={() => onButtonClick(element)}
-            >
-              {element.icon}
-              <span>{element.title}</span>
-            </ButtonToggle>
-          );
-        })}
-      </ButtonGroup>
+      <ButtonGroup>{renderTabButtons}</ButtonGroup>
       <RouteContentArea />
-      <div className='stepper-outer-box'>
-        <div className='stepper-inner-box'>
+      <div className="stepper-outer-box">
+        <div className="stepper-inner-box">
           <StepperComponent step={step} onButtonClick={onButtonClick} />
         </div>
       </div>
