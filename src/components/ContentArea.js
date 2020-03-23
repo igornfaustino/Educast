76f6ba825bfbcee87e47507cf35d941265e709b9
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2020-03-23 15:41:19 +0000

    add some initial progress

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Chapters from './Chapters'
 
 function RouteContentArea() {
+<<<<<<< HEAD
 	return (
 		<Switch>
 			<Route exact path="/editor" component={() => <div>editor</div>} />
@@ -12,6 +14,18 @@ function RouteContentArea() {
 			<Route path="*" component={() => <div>metadados</div>} />
 		</Switch>
 	);
+=======
+  return (
+    <Switch>
+      <Route exact path="/editor" component={() => <div>editor</div>} />
+      <Route exact path="/chapters" component={Chapters} />
+      <Route exact path="/documents" component={() => <div>Documentos</div>} />
+      <Route exact path="/branding" component={() => <div>Branding</div>} />
+      <Route exact path="/subtitles" component={() => <div>Legendas</div>} />
+      <Route path="*" component={() => <div>metadados</div>} />
+    </Switch>
+  );
+>>>>>>> add some initial progress
 }
 
 export default RouteContentArea;
