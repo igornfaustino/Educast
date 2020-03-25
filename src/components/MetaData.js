d3b205646636aaca1bfcb15d71dd59b30b32f80f
Gabriel Ferreira <gabriel97_felipe@hotmail.com>
2020-03-25 14:58:50 +0000

    Alterado componentes do textarea de comentários e tags no componente de metadata

--- src/components/MetaData.js
+++ src/components/MetaData.js
@@ -1,74 +1,41 @@
 import React, { useState } from 'react'
 
 import style from './MetaData.module.scss'
+import Labels from './Labels'
+import LabelComment from './LabelComment'
 
 
-function MetaData() {
+const MetaData = () => {
     const [data] = useState({
         titulo: 'Educast - grava, edita e publica',
         subtitulo: 'Grave as aulas com o Educast',
         data: '13 Agosto',
         local: 'Laboratório Nacional de Engenharia Civil',
-        comentario: 'Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário',
+        descricao: 'Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário',
         tag: 'Aula de React x',
     });
-
 
     return (
         <div>
             <form>
                 <div>
-                    
-                    <div className={style.container}>
 
-                        <label>
-                            <h4> Título: </h4>
-                            <h5>
-                                < input type="text" name="titulo"
-                                    value={data.titulo} />
-                            </h5>
-                        </label>
-                        <label>
-                            <h4 > Subtítulo: </h4>
-                            <h5>
-                                < input type="text" name="subtitulo" value={data.subtitulo} />
-                            </h5>
-                        </label>
-                        <label>
-                            <h4 > Data: </h4>
-                            <h5>
-                                < input type="text" name="data" value={data.data} />
-                            </h5>
-                        </label>
-                        <label>
-                            <h4 > Local: </h4>
-                            <h5>
-                                < input type="text" name="local" value={data.local} />
-                            </h5>
-                        </label>
+                    <div className={style.container}>
+                        <Labels title="Título" type="text" value={data.titulo} />
+                        <Labels title="Subtítulo" type="text" value={data.subtitulo} />
+                        <Labels title="Data" type="text" value={data.data} />
+                        <Labels title="Local" type="text" value={data.local} />
                     </div>
                     <div className={style.container}>
-                        <label>
-                            <h4> Comentários: </h4>
-                            <h5>
-                                < textarea type="textarea" name="comentarios">
-                                {data.comentario}
-                                </textarea>
-                            </h5>
-                        </label>
-                        <label>
-                            <h4> Tags: </h4>
-                            <h5>
-                                < input type="text" name="tag"
-                                    value={data.tag} />
-                            </h5>
-                        </label>
+                        <LabelComment title="Descrição" type="text" value={data.descricao} style={style["textarea-comment"]} minRows={3} maxRows={5} />                      
+                        <LabelComment title="Tag" type="text" value={data.tag} style={style["textarea-tag"]} minRows={1} maxRows={5} />
                     </div>
-                    
+
                     <div className={style.group}></div>
+
                 </div>
             </form>
-        </div>
+        </div >
     )
 }
 
