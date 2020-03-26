ebec943a52af1fcbb15e9232a8e1c9dd9a01f9c3
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2020-03-26 21:51:52 +0000

    some refactoring

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -3,18 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import Chapters from './Chapters'
 
 function RouteContentArea() {
-<<<<<<< HEAD
-	return (
-		<Switch>
-			<Route exact path="/editor" component={() => <div>editor</div>} />
-			<Route exact path="/chapters" component={() => <div>Capitulos</div>} />
-			<Route exact path="/documents" component={() => <div>Documentos</div>} />
-			<Route exact path="/branding" component={() => <div>Branding</div>} />
-			<Route exact path="/subtitles" component={() => <div>Legendas</div>} />
-			<Route path="*" component={() => <div>metadados</div>} />
-		</Switch>
-	);
-=======
   return (
     <Switch>
       <Route exact path="/editor" component={() => <div>editor</div>} />
@@ -25,7 +13,6 @@ function RouteContentArea() {
       <Route path="*" component={() => <div>metadados</div>} />
     </Switch>
   );
->>>>>>> add some initial progress
 }
 
 export default RouteContentArea;
