68546cb764b65662082214030f15fdeb24e85557
Eduardo Yuzo Nakai <yuzonakai@gmail.com>
2020-04-02 23:54:51 +0100

    progress on thumbnails

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -1,63 +1,70 @@
-import React, {useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { FaUpload, FaImages, FaTimes, FaChalkboardTeacher } from 'react-icons/fa';
-import Box from '@material-ui/core/Box';
-import EditableTextField from './EditableTextField'
-import styles from './CustomCard.module.css'
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import {
+  FaUpload,
+  FaImages,
+  FaTimes,
+  FaChalkboardTeacher,
+  FaImage
+} from "react-icons/fa";
+import Box from "@material-ui/core/Box";
+import EditableTextField from "./EditableTextField";
+import styles from "./CustomCard.module.css";
+import videojs from "video.js";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: '210px',
+    maxWidth: "210px"
   },
   media: {
     height: 140,
-    width: '100%',
-    position: 'relative',
+    width: "100%",
+    position: "relative"
   },
   title: {
-    fontSize: 17,
+    fontSize: 17
   },
   deleteButton: {
-    float: 'center',
-    maxWidth: '25px',
-    maxHeight: '25px',
-    minWidth: '25px',
-    minHeight: '25px',
-    paddingRight: '4px',
-    paddingTop: '3px',
-    color: '#12AADA',
-    background: 'white',
+    float: "center",
+    maxWidth: "25px",
+    maxHeight: "25px",
+    minWidth: "25px",
+    minHeight: "25px",
+    paddingRight: "4px",
+    paddingTop: "3px",
+    color: "#12AADA",
+    background: "white",
     "&:hover": {
       color: "#12AADA",
       background: "#EDD9FF"
     }
   },
   thumbnailButton: {
-    maxWidth: '30px',
-    maxHeight: '30px',
-    minWidth: '30px',
-    minHeight: '30px',
-    paddingRight: '4px',
-    paddingLeft: '15px',
-    color: '#12AADA',
-    background: 'white',
+    maxWidth: "30px",
+    maxHeight: "30px",
+    minWidth: "30px",
+    minHeight: "30px",
+    paddingRight: "4px",
+    paddingLeft: "15px",
+    color: "#12AADA",
+    background: "white",
     "&:hover": {
       color: "#12AADA",
       background: "#EDD9FF"
     }
   },
   cardHeader: {
-    background: '#12AADA',
-    color: 'white',
-    padding: '12px',
+    background: "#12AADA",
+    color: "white",
+    padding: "12px"
   },
   cardActions: {
-    background: '#F3E9FC'
+    background: "#F3E9FC"
   }
 });
 
@@ -65,11 +72,68 @@ const useStyles = makeStyles({
 function importAll(r) {
   return r.keys().map(r);
 }
-const images = importAll(require.context('../sampleImages/', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(
+  require.context("../sampleImages/", false, /\.(png|jpe?g|svg)$/)
+);
 
-const CustomCard = ({ chapter, deleteChapterFunction, updateTitleFunction }) => {
-  const [imageFile, setImageFile] = useState('');
+const CustomCard = ({
+  chapter,
+  deleteChapterFunction,
+  updateTitleFunction,
+  selectThumbnailFunction
+}) => {
+  const [imageFile, setImageFile] = useState("");
+  const [thumbnailImage, setThumbnailImage] = useState("");
   const classes = useStyles();
+  const [video, setVideo] = useState('');
+  // const video = document.querySelector("video");
+  // const canvas = document.querySelector("canvas");
+  // const context = canvas.getContext("2d");
+  // var w, h, ratio;
+  // //add loadedmetadata which will helps to identify video attributes......
+  // video.addEventListener(
+  //   "loadedmetadata",
+  //   function() {
+  //     ratio = video.videoWidth / video.videoHeight;
+  //     w = video.videoWidth - 100;
+  //     h = parseInt(w / ratio, 10);
+  //     canvas.width = w;
+  //     canvas.height = h;
+  //     console.log(w);
+  //   },
+  //   false
+  // );
+
+  useEffect(() => {
+    setThumbnailImage(images[0]); //select presentation snapshot by default
+    const video = document.querySelector('video');
+    const vv = videojs(video, function onPlayerReady() {
+      console.log('onPlayerReady')
+    });
+    
+    setVideo(vv);
+  }, []);
+
+  const handleThumbnailSelection = path => {
+    selectThumbnailFunction(chapter.id, path);
+    if (path === "primary") {
+      chapter.thumbnail = "primary";
+      setThumbnailImage(images[0]); //extract snapshot
+    } else if (path === "secondary") {
+      chapter.thumbnail = "secondary";
+      setThumbnailImage(images[1]); //extract snapshot
+    } else {
+      chapter.thumbnail = path;
+      setThumbnailImage(images[2]); //upload function/import image in path
+    }
+  };
+
+  const extractVideoSnapshot = () => {
+    //us
+    console.log("snapshoted");
+    // context.fillRect(0, 0, w, h);
+    // context.drawImage(video, 0, 0, w, h);
+  };
 
   return (
     <Card className={classes.root} square={true}>
@@ -86,50 +150,60 @@ const CustomCard = ({ chapter, deleteChapterFunction, updateTitleFunction }) => 
             variant="contained"
             color="secondary"
             className={classes.deleteButton}
-            startIcon={<FaTimes />} 
+            startIcon={<FaTimes />}
             onClick={deleteChapterFunction}
           />
         }
       />
       <CardMedia
         className={classes.media}
-        image={images[1]}
-        title={'burger'}
+        image={thumbnailImage}
+        title={"burger"}
       >
-        <Box
-          position="absolute"
-          top="5%"
-          left="85%"
-        >
-          <Button className={classes.thumbnailButton} startIcon={<FaImages />} />
+        <Box position="absolute" top="5%" left="85%">
+          <Button
+            className={classes.thumbnailButton}
+            startIcon={<FaImages />}
+            onClick={() => handleThumbnailSelection("primary")}
+          />
         </Box>
-        <Box
-          position="absolute"
-          top="30%"
-          left="85%"
-        >
-          <Button className={classes.thumbnailButton} startIcon={<FaChalkboardTeacher />} />
+        <Box position="absolute" top="30%" left="85%">
+          <Button
+            className={classes.thumbnailButton}
+            startIcon={<FaChalkboardTeacher />}
+            onClick={() => handleThumbnailSelection("secondary")}
+          />
         </Box>
-        <Box
-          position="absolute"
-          top="55%"
-          left="85%"
-        >
-          <Button key={chapter.id} className={classes.thumbnailButton} startIcon={<FaUpload />} />
+        <Box position="absolute" top="55%" left="85%">
+          <Button
+            key={chapter.id}
+            className={classes.thumbnailButton}
+            startIcon={<FaUpload />}
+            onClick={() => handleThumbnailSelection("upload")}
+          />
+        </Box>
+        <Box position="absolute" top="80%" left="85%">
+          <Button
+            key={chapter.id}
+            className={classes.thumbnailButton}
+            startIcon={<FaImage />}
+            onClick={() => extractVideoSnapshot()}
+          />
         </Box>
       </CardMedia>
       <div className={classes.cardActions}>
-        <div className={styles['CustomCard__TimeLabel']}>
+        <div className={styles["CustomCard__TimeLabel"]}>
           In {chapter.initTime}::{chapter.finalTime}
         </div>
-        <EditableTextField type='text'
-                           value={chapter.title}
-                           updateTitleFunction={updateTitleFunction}
-                           chapter={chapter}
+        <EditableTextField
+          type="text"
+          value={chapter.title}
+          updateTitleFunction={updateTitleFunction}
+          chapter={chapter}
         />
       </div>
     </Card>
   );
-}
+};
 
-export default CustomCard
+export default CustomCard;
--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,23 +1,98 @@
-import React, { useState } from "react";
-import ImageUploader from "react-images-upload";
+// import React, { Component } from "react";
+// import styles from "./ImageUpload.module.css";
+import React, { useState } from 'react';
+import './ImageUpload.module.css';
 
-const ImageUpload = ({ props }) => {
-  const [pictures, setPictures] = useState([]);
+// class ImageUpload extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { hightlight: false };
+//     this.fileInputRef = React.createRef();
 
-  const onDrop = (picture) => {
-    setPictures(pictures.concat(picture));
-    console.log(pictures)
-  };
-
-  return (
-    <ImageUploader
-      {...props}
-      withIcon={true}
-      onChange={onDrop}
-      imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-      maxFileSize={10000000}
-    />
-  );
-};
+const ImageUpload = ({ handleFilesAddition }) => {
+  const openFileDialog = () => {
+    
+  }
+}
 
 export default ImageUpload;
+
+//     this.openFileDialog = this.openFileDialog.bind(this);
+//     this.onFilesAdded = this.onFilesAdded.bind(this);
+//     this.onDragOver = this.onDragOver.bind(this);
+//     this.onDragLeave = this.onDragLeave.bind(this);
+//     this.onDrop = this.onDrop.bind(this);
+//   }
+
+//   openFileDialog() {
+//     if (this.props.disabled) return;
+//     this.fileInputRef.current.click();
+//   }
+
+//   onFilesAdded(evt) {
+//     if (this.props.disabled) return;
+//     const files = evt.target.files;
+//     if (this.props.onFilesAdded) {
+//       const array = this.fileListToArray(files);
+//       this.props.onFilesAdded(array);
+//     }
+//   }
+
+//   onDragOver(event) {
+//     event.preventDefault();
+//     if (this.props.disabed) return;
+//     this.setState({ hightlight: true });
+//   }
+
+//   onDragLeave(event) {
+//     this.setState({ hightlight: false });
+//   }
+
+//   onDrop(event) {
+//     event.preventDefault();
+//     if (this.props.disabed) return;
+//     const files = event.dataTransfer.files;
+//     if (this.props.onFilesAdded) {
+//       const array = this.fileListToArray(files);
+//       this.props.onFilesAdded(array);
+//     }
+//     this.setState({ hightlight: false });
+//   }
+
+//   fileListToArray(list) {
+//     const array = [];
+//     for (var i = 0; i < list.length; i++) {
+//       array.push(list.item(i));
+//     }
+//     return array;
+//   }
+
+//   render() {
+//     return (
+//       <div
+//         className={`Dropzone ${this.state.hightlight ? "Highlight" : ""}`}
+//         onDragOver={this.onDragOver}
+//         onDragLeave={this.onDragLeave}
+//         onDrop={this.onDrop}
+//         onClick={this.openFileDialog}
+//         style={{ cursor: this.props.disabled ? "default" : "pointer" }}
+//       >
+//         <input
+//           ref={this.fileInputRef}
+//           className={styles["FileInput"]}
+//           type="file"
+//           multiple
+//           onChange={this.onFilesAdded}
+//         />
+//         <img
+//           alt="upload"
+//           className="Icon"
+//           src="baseline-cloud_upload-24px.svg"
+//         />
+//         <span>Upload Files</span>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ImageUpload;
