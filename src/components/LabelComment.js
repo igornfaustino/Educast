9f8a4dd477820df284499bcd66c8ab5e70ef9108
Gabriel Ferreira <gabriel97_felipe@hotmail.com>
2020-04-20 17:55:52 +0100

    Inserido chips do material.

--- src/components/LabelComment.js
+++ src/components/LabelComment.js
@@ -1,28 +1,84 @@
 
 import React, { useState } from 'react';
+import * as Showdown from "showdown";
+import ReactMde from "react-mde";
 
 import TextareaAutosize from 'react-textarea-autosize';
-import style from './LabelComment.module.scss'
+import style from './LabelComment.module.scss';
+import 'react-mde/lib/styles/css/react-mde-all.css';
 
 const LabelComment = props => {
+    const loadSuggestions = text => {
+        return new Promise((accept, reject) => {
+            setTimeout(() => {
+                const suggestions = [
+                    {
+                        preview: "Andre",
+                        value: "@andre"
+                    },
+                    {
+                        preview: "Angela",
+                        value: "@angela"
+                    },
+                    {
+                        preview: "David",
+                        value: "@david"
+                    },
+                    {
+                        preview: "Louise",
+                        value: "@louise"
+                    }
+                ].filter(i => i.preview.toLowerCase().includes(text.toLowerCase()));
+                accept(suggestions);
+            }, 250);
+        });
+    }
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true
+    });
+
     const [profileState] = useState(props);
-    console.log(profileState.style);
+    const [value, setValue] = React.useState("**Hello world!!!**");
+    const [selectedTab, setSelectedTab] = React.useState("write");
     return (
         <div>
             <label>
                 <h4> {profileState.title} </h4>
-                <h5>
 
-                    <div className={style["img"]}></div>
+                <div className={profileState.style[1]}>
+                    <h5>
+                        {/* <ReactMde
+                            className={profileState.style[0]}
+                            value={value}
+                            onChange={setValue}
+                            selectedTab={selectedTab}
+                            onTabChange={setSelectedTab}
+                            generateMarkdownPreview={markdown =>
+                                Promise.resolve(converter.makeHtml(markdown))
+                            }
+                            loadSuggestions={loadSuggestions}
+                            childProps={{
+                                writeButton: {
+                                    tabIndex: -1
+                                }
+                            }}
+                        /> */}
+                        <div className={style["img"]}></div>
                     < TextareaAutosize
                         type={profileState.type}
                         name={profileState.title}
                         defaultValue={profileState.value}
-                        className={profileState.style}
+                        className={profileState.style[0]}
                         minRows={profileState.minRows}
                         maxRows={profileState.maxRows}
                     />
-                </h5>
+                    </h5>
+                </div>
+
             </label>
         </div>
 
