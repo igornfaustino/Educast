c8c423e2aef994f52591cd52b6c68d7f53dcaadf
Gabriel Ferreira <gabriel97_felipe@hotmail.com>
2020-04-20 15:37:40 +0100

    Edição dos componentes Chip e Comment

--- src/components/LabelComment.js
+++ src/components/LabelComment.js
@@ -2,14 +2,18 @@
 import React, { useState } from 'react';
 
 import TextareaAutosize from 'react-textarea-autosize';
+import style from './LabelComment.module.scss'
 
 const LabelComment = props => {
     const [profileState] = useState(props);
+    console.log(profileState.style);
     return (
         <div>
             <label>
                 <h4> {profileState.title} </h4>
                 <h5>
+
+                    <div className={style["img"]}></div>
                     < TextareaAutosize
                         type={profileState.type}
                         name={profileState.title}
