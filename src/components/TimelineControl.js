137cb47a5fa5b90a8914125277def902eeecb1af
igornfaustino <igornfaustino@gmail.com>
2020-05-07 11:29:37 +0100

    [re-factory] re-factory timeline control

--- src/components/TimelineControl.js
+++ src/components/TimelineControl.js
@@ -1,9 +1,10 @@
-import React, { forwardRef, useRef } from 'react';
+import React, { forwardRef, useRef, useCallback } from 'react';
 import { MdZoomIn, MdZoomOut } from 'react-icons/md';
 
 import { ZOOM_MAX } from '../utils/constants';
 
 import styles from './TimelineControl.module.scss';
+import { useMemo } from 'react';
 
 const TimelineControl = (
 	{ timerDivWidth, zoom, setZoom },
@@ -11,34 +12,46 @@ const TimelineControl = (
 ) => {
 	const mainScrollbarRef = useRef(null);
 
-	const handleScroll = () => {
+	const handleScroll = useCallback(() => {
 		if (!videoTimelineRef.current) return;
 		videoTimelineRef.current.scrollLeft = mainScrollbarRef.current.scrollLeft;
-	};
+	}, [videoTimelineRef]);
 
-	return (
-		<div className={styles['scrollbar']}>
-			<div className={styles['blackbox']}></div>
+	const handleZoomChange = useCallback(
+		(evt) => {
+			setZoom(evt.target.value);
+		},
+		[setZoom]
+	);
 
+	const scrollStyle = useMemo(
+		() => ({
+			height: '100%',
+			width: timerDivWidth + 'px',
+			backgroundColor: 'transparent',
+		}),
+		[timerDivWidth]
+	);
+
+	const scrollBar = useMemo(
+		() => (
 			<div className={styles['scrollbar__container']}>
 				<div
 					className={styles['timeline__scrollbar']}
 					ref={mainScrollbarRef}
 					onScroll={handleScroll}
 				>
-					<div
-						style={{
-							height: '100%',
-							width: timerDivWidth + 'px',
-							backgroundColor: 'transparent',
-						}}
-						className="timeline__video-invisible"
-					/>
+					<div style={scrollStyle} className="timeline__video-invisible" />
 				</div>
 
 				<div className={styles['scrollbar__blackbox']}></div>
 			</div>
+		),
+		[handleScroll, scrollStyle]
+	);
 
+	const zoomInput = useMemo(
+		() => (
 			<div className={styles['zoom__bar']}>
 				<MdZoomOut className={styles['btnContainer__icon']} />
 				<div className={styles['slider-box']}>
@@ -47,9 +60,7 @@ const TimelineControl = (
 						min="1"
 						max={ZOOM_MAX}
 						value={zoom}
-						onChange={(evt) => {
-							setZoom(evt.target.value);
-						}}
+						onChange={handleZoomChange}
 						step="1"
 						className={styles['slider']}
 						id="myRange"
@@ -57,6 +68,17 @@ const TimelineControl = (
 				</div>
 				<MdZoomIn className={styles['btnContainer__icon']} />
 			</div>
+		),
+		[handleZoomChange, zoom]
+	);
+
+	return (
+		<div className={styles['scrollbar']}>
+			<div className={styles['blackbox']}></div>
+
+			{scrollBar}
+
+			{zoomInput}
 		</div>
 	);
 };
