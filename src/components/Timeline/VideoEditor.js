3467e5d4ef048fcfd6baf6587051d33650bf69be
caio nakai <kaioesperto@hotmail.com>
2020-05-25 20:37:29 +0100

    [shortcut] add video timeline shortcut

--- src/components/Timeline/VideoEditor.js
+++ src/components/Timeline/VideoEditor.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from 'react';
-
 import { useSelector } from 'react-redux';
-
+import { useHotkeys } from 'react-hotkeys-hook';
 import Timeline from './Timeline';
 import TimelineControl from './TimelineControl';
 import { getNumberOfMainIndicators } from '../../utils/conversions';
@@ -21,6 +20,7 @@ const VideoEditor = ({
 
 	const duration = useSelector((state) => state.video.duration);
 	const visibleArea = useSelector((state) => state.timeline.visibleArea);
+	const currentTime = useSelector((state) => state.video.currentTime);
 
 	const [videoLength, setVideoLength] = useState(duration);
 	const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
@@ -33,6 +33,49 @@ const VideoEditor = ({
 	);
 	const [calculatedMargin, setCalculatedMargin] = useState(
 		BASE_TIME_INDICATOR_MARGIN
+	);
+
+	useHotkeys(
+		'left',
+		() => {
+			const frameSize = 1 / 25;
+			const currentFrame = Math.round(currentTime / frameSize);
+			const previousFramePosition = (currentFrame - 1) * frameSize;
+			handleTimelineClick(previousFramePosition);
+		},
+		{},
+		[currentTime]
+	);
+
+	useHotkeys(
+		'right',
+		() => {
+			const frameSize = 1 / 25;
+			const currentFrame = Math.round(currentTime / frameSize);
+			const nextFramePosition = (currentFrame + 1) * frameSize;
+			console.log({ currentFrame, nextFramePosition });
+			handleTimelineClick(nextFramePosition);
+		},
+		{},
+		[currentTime]
+	);
+
+	useHotkeys(
+		'shift+right',
+		() => {
+			handleTimelineClick(parseInt(currentTime) + 1);
+		},
+		{},
+		[currentTime]
+	);
+
+	useHotkeys(
+		'shift+left',
+		() => {
+			handleTimelineClick(parseInt(currentTime) - 1);
+		},
+		{},
+		[currentTime]
 	);
 
 	const getRealTimeIndicatorMargin = useCallback(() => {
--- src/utils/conversions.js
+++ src/utils/conversions.js
@@ -3,6 +3,7 @@ export const getPositionInPx = (percent, timerDivWidth) =>
 
 export const getPositionInPercent = (px, timerDivWidth) => px / timerDivWidth;
 
+// https://www.infoescola.com/fisica/conversao-de-escalas-termometricas/
 export const getNumberOfMainIndicators = (zoomLevel, duration) => {
 	return ((zoomLevel - 1) / 9) * (duration - 10) + 10;
 };
