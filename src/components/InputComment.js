e5338ee4364792b0db7cbd6e11de6c611f7d7178
Gabriel Ferreira <gabriel97_felipe@hotmail.com>
2020-06-04 15:12:46 +0100

    Controlando estados dos componentes InputFields, InputComment e InputChip

---
+++ src/components/InputComment.js
@@ -0,0 +1,68 @@
+import React, { useMemo, useCallback } from "react";
+import * as Showdown from "showdown";
+import ReactMde from "react-mde";
+
+import style from "./InputComment.module.scss";
+import "./react-mde-all.scss"
+
+const InputComment = ({ title, type, value }) => {
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true
+    });
+
+
+    const inputClasses = useMemo(() => ({
+        reactMde: style["react-mde"],
+        textArea: style["text-area"],
+        preview: style["preview"],
+        toolbar: style["toolbar"],
+        grip: style["grip"],
+    }), []);
+
+
+    const childProps = useMemo(() => ({
+        writeButton: {
+            tabIndex: -1
+        }
+    }), []);
+
+    const handlerMarkdownPreview = useCallback(
+        markdown =>
+            Promise.resolve(converter.makeHtml(markdown))
+    );
+
+    const [description, setDescription] = React.useState(value);
+    const [selectedTab, setSelectedTab] = React.useState("write");
+    return (
+        <div>
+            <label>
+                <h4> {title} </h4>
+
+                <div className={style["contentarea-comment"]}>
+
+                    <ReactMde
+                        className={style["textarea-comment"]}
+                        value={description}
+                        onChange={setDescription}
+                        selectedTab={selectedTab}
+                        minEditorHeight="100px"
+                        minPreviewHeight="100px"
+                        maxEditorHeight="100px"
+                        onTabChange={setSelectedTab}
+                        generateMarkdownPreview={handlerMarkdownPreview}
+                        childProps={childProps}
+                        classes={inputClasses}
+                    />
+                </div>
+            </label>
+        </div>
+
+    );
+};
+
+export default InputComment;
+
--- src/components/MetaData.js
+++ src/components/MetaData.js
@@ -1,35 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState} from "react"
 
-import style from './MetaData.module.scss'
-import Labels from './Labels'
-import LabelComment from './LabelComment'
-import LabelChip from './LabelChip'
+import style from "./MetaData.module.scss"
+import InputFields from "./InputFields"
+import InputComment from "./InputComment"
+import InputChip from "./InputChip"
 
 
 const MetaData = () => {
     const [data] = useState({
-        titulo: 'Educast - grava, edita e publica',
-        subtitulo: 'Grave as aulas com o Educast',
-        data: '13 Agosto',
-        local: 'Laboratório Nacional de Engenharia Civil',
-        descricao: 'Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário',
-        tag: ['Aula de React'],
+        title: "Educast - grava, edita e publica",
+        subtitle: "Grave as aulas com o Educast",
+        date: "13 Agosto",
+        local: "Laboratório Nacional de Engenharia Civil",
+        description: "Comentário",
+        tag: ["Aula de React"],
     });
 
+
     return (
-        <div>
+        <div className={style.metadata}>
             <form>
                 <div>
 
                     <div className={style.container}>
-                        <Labels title="Título" type="text" value={data.titulo} />
-                        <Labels title="Subtítulo" type="text" value={data.subtitulo} />
-                        <Labels title="Data" type="text" value={data.data} />
-                        <Labels title="Local" type="text" value={data.local} />
+                        <InputFields title="Título" type="text" value={data.title} />
+                        <InputFields title="Subtítulo" type="text" value={data.subtitle} />
+                        <InputFields title="Data" type="text" value={data.date} />
+                        <InputFields title="Local" type="text" value={data.local} />
                     </div>
                     <div className={style.container}>
-                        <LabelComment title="Descrição" type="text" value={data.descricao} style={[style["textarea-comment"], style["contentarea-comment"]]} minRows={4} maxRows={7} />                      
-                        <LabelChip title="Tag" type="text" value={data.tag}  minRows={1} maxRows={5} />
+                        <InputComment title="Descrição" type="text" value={data.description}/>
+                        <InputChip title="Tag" type="text" value={data.tag} />
                     </div>
 
                     <div className={style.group}></div>
