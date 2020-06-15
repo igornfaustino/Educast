32b0fd94d3c59b7960f62db6729eb297879efed5
igornfaustino <igornfaustino@gmail.com>
2020-06-15 15:15:32 -0300

    [review] add useMemo to modal files

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Modal as ModalBootstrap, ModalHeader, ModalBody } from 'reactstrap';
 import { FaWindowClose } from 'react-icons/fa';
 import styles from './Modal.module.scss';
 
 const Modal = ({ onToggle, isOpen, title, body, isClosable, size = 'lg' }) => {
+	const toggle = useMemo(() => (isClosable ? onToggle : undefined), [
+		isClosable,
+		onToggle,
+	]);
+
+	const closeIcon = useMemo(
+		() =>
+			isClosable ? (
+				<FaWindowClose
+					size="1.3rem"
+					className={styles.closeIcon}
+					onClick={onToggle}
+				/>
+			) : undefined,
+		[isClosable, onToggle]
+	);
+
 	return (
 		<div>
 			<ModalBootstrap
@@ -13,17 +30,9 @@ const Modal = ({ onToggle, isOpen, title, body, isClosable, size = 'lg' }) => {
 				contentClassName={styles.modal}
 			>
 				<ModalHeader
-					toggle={isClosable ? onToggle : undefined}
+					toggle={toggle}
 					className={styles.modalHeader}
-					close={
-						isClosable ? (
-							<FaWindowClose
-								size="1.3rem"
-								className={styles.closeIcon}
-								onClick={onToggle}
-							/>
-						) : undefined
-					}
+					close={closeIcon}
 				>
 					{title}
 				</ModalHeader>
--- src/components/PublishModal.js
+++ src/components/PublishModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './PublishModal.module.scss';
 import Modal from './Modal';
 
@@ -7,43 +7,49 @@ import { FaRegImages } from 'react-icons/fa';
 import { Button } from 'reactstrap';
 
 const SaveModal = ({ onToggle, isOpen }) => {
-	const body = (
-		<>
-			<p className={styles.text}>
-				O clip será exportado para seu canal
-				<br />
-				em vários formatos de entrega:
-			</p>
-			<div className={styles.iconsWrapper}>
-				<div className={styles.iconTextWrapper}>
-					<FaRegImages size="2rem" />
-					<span>Streaming</span>
+	const body = useMemo(
+		() => (
+			<>
+				<p className={styles.text}>
+					O clip será exportado para seu canal
+					<br />
+					em vários formatos de entrega:
+				</p>
+				<div className={styles.iconsWrapper}>
+					<div className={styles.iconTextWrapper}>
+						<FaRegImages size="2rem" />
+						<span>Streaming</span>
+					</div>
+					<div className={styles.iconTextWrapper}>
+						<FaRegImages size="2rem" />
+						<span>Desktop</span>
+					</div>
+					<div className={styles.iconTextWrapper}>
+						<FaRegImages size="2rem" />
+						<span>Mobile</span>
+					</div>
 				</div>
-				<div className={styles.iconTextWrapper}>
-					<FaRegImages size="2rem" />
-					<span>Desktop</span>
+				<div className={styles.button}>
+					<Button
+						color="primary"
+						className="app-buttons  app-home-buttons"
+						onClick={onToggle}
+					>
+						OK
+					</Button>
 				</div>
-				<div className={styles.iconTextWrapper}>
-					<FaRegImages size="2rem" />
-					<span>Mobile</span>
-				</div>
-			</div>
-			<div className={styles.button}>
-				<Button
-					color="primary"
-					className="app-buttons  app-home-buttons"
-					onClick={onToggle}
-				>
-					OK
-				</Button>
-			</div>
-		</>
+			</>
+		),
+		[onToggle]
 	);
 
-	const title = (
-		<div className={styles.titleWrapper}>
-			<span>Publicação</span>
-		</div>
+	const title = useMemo(
+		() => (
+			<div className={styles.titleWrapper}>
+				<span>Publicação</span>
+			</div>
+		),
+		[]
 	);
 
 	return (
