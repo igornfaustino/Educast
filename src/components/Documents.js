55586cba59027eb230d0bfc1a714ba29b7df7587
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2020-06-22 19:11:15 +0100

    changes to use useCallback, useMemo and updated way to import styles

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import './Documents.scss';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import styles from './Documents.module.scss';
 import { FaLink } from 'react-icons/fa';
 import { AiFillFolderOpen } from 'react-icons/ai';
 import { BsTrashFill } from 'react-icons/bs';
 import { TiEdit } from 'react-icons/ti';
 import axios from 'axios';
+import cx from 'classnames';
 
 const apiUrlDocuments = 'http://localhost:8080/documents/';
 
@@ -16,11 +17,7 @@ function Documents() {
 	const [editingUrl, setEditingUrl] = useState('')
 	const [isEditingUrl, setIsEditingUrl] = useState(false)
 
-	useEffect(() => {
-		getDocuments()
-	}, []);
-
-	const getDocuments = () => {
+	const getDocuments = useCallback(() => {
 		axios
 			.get(apiUrlDocuments).then((response) => {
 				const formattedFiles = response.data.data.map((file) => {
@@ -30,9 +27,13 @@ function Documents() {
 			}).catch(_ => {
 				alert("Ocorreu um erro tentando obter os documentos")
 			});
-	}
+	}, []);
 
-	const deleteDocument = (documentTitle) => {
+	useEffect(() => {
+		getDocuments()
+	}, [getDocuments]);
+
+	const deleteDocument = useCallback((documentTitle) => {
 		axios
 			.delete(apiUrlDocuments, { data: { name: documentTitle } })
 			.then((response) => {
@@ -45,13 +46,13 @@ function Documents() {
 				alert("Ocorreu um erro ao deletar o documento")
 
 			})
-	};
+	}, [documents]);
 
-	const editDocument = (oldTitle, newTitle, callback) => {
+	const editDocument = useCallback((oldTitle, newTitle, callback) => {
 		axios
 			.put(apiUrlDocuments, { oldName: oldTitle, newName: newTitle })
 			.then((res) => {
-				if (res.status == 200) {
+				if (res.status === 200) {
 					callback();
 				} else {
 					alert("Ocorreu um erro ao editar o documento")
@@ -59,13 +60,31 @@ function Documents() {
 			}).catch(_ => {
 				alert("Ocorreu um erro ao editar o documento")
 			});
-	};
+	}, []);
 
-	const downloadFile = (filename) => {
+	const downloadFile = useCallback((filename) => {
 		window.open(`${apiUrlDocuments}${filename}`)
-	}
+	}, []);
 
-	const uploadFile = (event) => {
+	const upload = useCallback((file) => {
+		var formData = new FormData();
+		formData.append("data", file);
+		axios.post(apiUrlDocuments, formData, {
+			headers: {
+				"Content-Type": "multipart/form-data"
+			}
+		}).then((res) => {
+			if (res.status === 200) {
+				getDocuments()
+			} else {
+				alert("Ocorreu um erro ao fazer upload do documento")
+			}
+		}).catch(_ => {
+			alert("Ocorreu um erro ao fazer upload do documento")
+		})
+	}, [getDocuments]);
+
+	const uploadFile = useCallback((event) => {
 		event.preventDefault()
 		if (event.dataTransfer.items) {
 			for (var i = 0; i < event.dataTransfer.items.length; i++) {
@@ -75,76 +94,64 @@ function Documents() {
 				}
 			}
 		}
-	}
+	}, [upload]);
 
-	const upload = (file) => {
-		var formData = new FormData();
-		formData.append("data", file);
-		axios.post(apiUrlDocuments, formData, {
-			headers: {
-				"Content-Type": "multipart/form-data"
-			}
-		}).then((res) => {
-			if (res.status == 200) {
-				getDocuments()
-			} else {
-				alert("Ocorreu um erro ao fazer upload do documento")
-			}
-		}).catch(_ => {
-			alert("Ocorreu um erro ao fazer upload do documento")
-		})
-	}
-
-
-	const onDragOver = (event) => {
+	const onDragOver = useCallback((event) => {
 		event.preventDefault()
 		event.stopPropagation()
-	}
+	}, []);
 
-	const onChangeFileName = (event) => {
+	const onChangeFileName = useCallback((event) => {
 		if (event !== null && event !== '') {
 			setActualFilename(event.target.value);
 		}
-	};
+	}, []);
 
-	const onChangeUrl = (event) => {
+	const onChangeUrl = useCallback((event) => {
 		if (event !== null && event !== '') {
 			setEditingUrl(event.target.value);
 		}
-	};
+	}, []);
 
-	const editInput = (filename) => {
+	const editInput = useCallback((filename) => {
 		setExtensionFilename(filename.slice(filename.length - 4))
 		setActualFilename(filename.substring(0, filename.length - 4));
 		setEditingFilename(filename)
-	};
+	}, []);
 
-	const editUrl = (filename) => {
+	const editUrl = useCallback((filename) => {
 		setEditingUrl('');
 		setIsEditingUrl(true)
-	};
+	}, []);
 
-	const onEditInputKeyDown = (event) => {
+	const stopEditing = useCallback(() => {
+		setEditingFilename('');
+	}, []);
+
+	const submitEditing = useCallback((event) => {
+		const oldName = event.target.id
+		const newName = `${event.target.value}${extensionFilename}`
+		editDocument(oldName, newName, () => {
+			setEditingFilename('');
+			setDocuments(
+				documents.map((doc) => {
+					return doc.title === oldName ? { title: newName } : { title: doc.title };
+				})
+			);
+		});
+	}, [documents, editDocument, extensionFilename]);
+
+	const onEditInputKeyDown = useCallback((event) => {
 		var code = event.charCode || event.keyCode;
 		if (code === 27) stopEditing();
 		if (code === 13) submitEditing(event);
-	};
+	}, [stopEditing, submitEditing]);
 
-	const onEditUrlKeydown = (event) => {
-		var code = event.charCode || event.keyCode;
-		if (code === 27) stopEditingUrl();
-		if (code === 13) submitEditingUrl(event);
-	};
-
-	const stopEditing = () => {
-		setEditingFilename('');
-	};
-
-	const stopEditingUrl = () => {
+	const stopEditingUrl = useCallback(() => {
 		setIsEditingUrl(false);
-	};
+	}, []);
 
-	const submitEditingUrl = (event) => {
+	const submitEditingUrl = useCallback((event) => {
 		const url = event.target.value
 		fetch(url).then((res) => {
 			const header = res.headers.get('Content-Disposition')
@@ -162,26 +169,19 @@ function Documents() {
 			alert('Não foi possível acessar o URL')
 		})
 		setIsEditingUrl(false)
-	};
+	}, [upload]);
 
-	const submitEditing = (event) => {
-		const oldName = event.target.id
-		const newName = `${event.target.value}${extensionFilename}`
-		editDocument(oldName, newName, () => {
-			setEditingFilename('');
-			setDocuments(
-				documents.map((doc) => {
-					return doc.title === oldName ? { title: newName } : { title: doc.title };
-				})
-			);
-		});
-	};
+	const onEditUrlKeydown = useCallback((event) => {
+		var code = event.charCode || event.keyCode;
+		if (code === 27) stopEditingUrl();
+		if (code === 13) submitEditingUrl(event);
+	}, [stopEditingUrl, submitEditingUrl]);
 
-	const renderFilename = (document) => {
+	const renderFilename = useCallback((document) => {
 		if (editingFilename === document.title) {
 			return (
 				<input
-					className="text list-documents-input-item"
+					className={cx(styles["text"], styles["list-documents-input-item"])} //"text list-documents-input-item"
 					value={actualFilename}
 					id={document.title}
 					autoFocus={true}
@@ -193,20 +193,20 @@ function Documents() {
 			return (
 				<div
 					onClick={() => downloadFile(document.title)}
-					className="list-documents-item-name"
+					className={styles["list-documents-item-name"]}
 					id={document.title}
 				>
-					<span className="text">{document.title}</span>
+					<span className={styles["text"]}>{document.title}</span>
 				</div>
 			);
 		}
-	};
+	}, [actualFilename, downloadFile, editingFilename, onChangeFileName, onEditInputKeyDown]);
 
-	const renderAddByUrl = () => {
+	const renderAddByUrl = useMemo(() => {
 		if (isEditingUrl) {
 			return (
 				<input
-					className="link-upload-container"
+					className={styles["link-upload-container"]}
 					value={editingUrl}
 					autoFocus={true}
 					onChange={(event) => onChangeUrl(event)}
@@ -216,63 +216,67 @@ function Documents() {
 		} else {
 			return (
 				<div
-					className="link-upload-text-container link-upload-container"
+					className={cx(styles["link-upload-text-container"], styles["link-upload-container"])}
 					onClick={() => editUrl()}
 				>
-					<span className="text">Adicionar o URL de um ficheiro</span>
+					<span className={styles["text"]}>Adicionar o URL de um ficheiro</span>
 				</div>
 			)
 		}
-	}
+	}, [editUrl, editingUrl, isEditingUrl, onChangeUrl, onEditUrlKeydown]);
+
+	const renderFiles = useMemo(() => {
+		return documents.map((document) => {
+			return (
+				<div key={document.title} className={styles["list-documents-item"]}>
+					{renderFilename(document)}
+					<BsTrashFill
+						size="1.75rem"
+						color="#d0d0d0"
+						className={styles["trash-icon"]}
+						onClick={() => deleteDocument(document.title)}
+						cursor="pointer"
+					/>
+					<TiEdit
+						size="1.75rem"
+						color="#d0d0d0"
+						onClick={() => editInput(document.title)}
+						cursor="pointer"
+					/>
+				</div>
+			);
+		})
+	}, [documents, deleteDocument, editInput, renderFilename]);
 
 	return (
-		<div className="general-container">
-			<div className="side left">
-				<div className="link-upload-container">
-					<div className="link-icon-container">
-						<FaLink size="1.25rem" color="white" className="link-icon" />
+		<div className={styles["general-container"]}>
+			<div className={cx(styles["side"], styles["left"])}>
+				<div className={styles["link-upload-container"]}>
+					<div className={styles["link-icon-container"]}>
+						<FaLink size="1.25rem" color="white" className={styles["link-icon"]} />
 					</div>
-					{renderAddByUrl()}
+					{renderAddByUrl}
 				</div>
-				<div className="or-text-container">
-					<span className="text">ou</span>
+				<div className={styles["or-text-container"]}>
+					<span className={styles["text"]}>ou</span>
 				</div>
-				<div className="upload-file-container"
+				<div className={styles["upload-file-container"]}
 					onDrop={(event) => uploadFile(event)}
 					onDragOver={(event) => onDragOver(event)}>
-					<div className="folder-icon">
+					<div className={styles["folder-icon"]}>
 						<AiFillFolderOpen size="3.0rem" color="#0099ff" />
 					</div>
-					<span className="text text-center">Arraste e solte seus documentos</span>
+					<span className={cx(styles["text"], styles["text-center"])}>Arraste e solte seus documentos</span>
 				</div>
 			</div>
-			<div className="side right">
-				<div className="list-documents-title-container">
-					<span className="list-documents-title-text text">
+			<div className={cx(styles["side"],  styles["right"])}>
+				<div className={styles["list-documents-title-container"]}>
+					<span className={cx(styles["list-documents-title-text"], styles["text"])}>
 						Lista de documentos adicionados
 					</span>
 				</div>
-				<div className="list-documents-container">
-					{documents.map((document) => {
-						return (
-							<div key={document.title} className="list-documents-item">
-								{renderFilename(document)}
-								<BsTrashFill
-									size="1.75rem"
-									color="#d0d0d0"
-									className="trash-icon"
-									onClick={() => deleteDocument(document.title)}
-									cursor="pointer"
-								/>
-								<TiEdit
-									size="1.75rem"
-									color="#d0d0d0"
-									onClick={() => editInput(document.title)}
-									cursor="pointer"
-								/>
-							</div>
-						);
-					})}
+				<div className={styles["list-documents-container"]}>
+					{renderFiles}
 				</div>
 			</div>
 		</div>
