055e9bec2ee3382e9bba8b995cb3cae07285bfca
Gabriel Ferreira <gabriel97_felipe@hotmail.com>
2020-07-03 22:34:13 +0100

    REsolvendo conflitos.

--- src/components/MetaData.js
+++ src/components/MetaData.js
@@ -1,36 +1,35 @@
-import React, { useState} from "react"
+import React, { useState } from 'react'
 
-import style from "./MetaData.module.scss"
-import InputFields from "./InputFields"
-import InputComment from "./InputComment"
-import InputChip from "./InputChip"
+import style from './MetaData.module.scss'
+import Labels from './Labels'
+import LabelComment from './LabelComment'
+import LabelChip from './LabelChip'
 
 
 const MetaData = () => {
     const [data] = useState({
-        title: "Educast - grava, edita e publica",
-        subtitle: "Grave as aulas com o Educast",
-        date: "13 Agosto",
-        local: "Laboratório Nacional de Engenharia Civil",
-        description: "Comentário",
-        tag: ["Aula de React"],
+        titulo: 'Educast - grava, edita e publica',
+        subtitulo: 'Grave as aulas com o Educast',
+        data: '13 Agosto',
+        local: 'Laboratório Nacional de Engenharia Civil',
+        descricao: 'Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário Comentário',
+        tag: ['Aula de React'],
     });
 
-
     return (
-        <div className={style.metadata}>
+        <div>
             <form>
                 <div>
 
                     <div className={style.container}>
-                        <InputFields title="Título" type="text" value={data.title} />
-                        <InputFields title="Subtítulo" type="text" value={data.subtitle} />
-                        <InputFields title="Data" type="text" value={data.date} />
-                        <InputFields title="Local" type="text" value={data.local} />
+                        <Labels title="Título" type="text" value={data.titulo} />
+                        <Labels title="Subtítulo" type="text" value={data.subtitulo} />
+                        <Labels title="Data" type="text" value={data.data} />
+                        <Labels title="Local" type="text" value={data.local} />
                     </div>
                     <div className={style.container}>
-                        <InputComment title="Descrição" type="text" value={data.description}/>
-                        <InputChip title="Tag" type="text" value={data.tag} />
+                        <LabelComment title="Descrição" type="text" value={data.descricao} style={[style["textarea-comment"], style["contentarea-comment"]]} minRows={4} maxRows={7} />                      
+                        <LabelChip title="Tag" type="text" value={data.tag}  minRows={1} maxRows={5} />
                     </div>
 
                     <div className={style.group}></div>
