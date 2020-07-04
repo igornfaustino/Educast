f2746953eadb0160b37e82f4128b7e76017b3b5a
igornfaustino <igornfaustino@gmail.com>
2020-07-04 15:07:34 -0300

    [warnings] remove warnings

--- src/components/MetaData/InputFields.js
+++ src/components/MetaData/InputFields.js
@@ -1,28 +1,21 @@
-import React, { useState } from "react";
+import React from 'react';
 
-const InputFields = ({ title, type, value, fowardedRef }) => {
-
-
-    const [inputValue, setInputValue] = useState(value);
-
-    return (
-        <div>
-            <label>
-                <h4> {title} </h4>
-                <h5>
-                    < input
-                        type={type}
-                        name={title}
-                        defaultValue={inputValue}
-                        onChange={value}
-                        ref={fowardedRef}
-                    />
-                </h5>
-            </label>
-        </div>
-
-    );
+const InputFields = ({ title, type, value }) => {
+	return (
+		<div>
+			<label>
+				<h4> {title} </h4>
+				<h5>
+					<input
+						type={type}
+						name={title}
+						defaultValue={value}
+						onChange={value}
+					/>
+				</h5>
+			</label>
+		</div>
+	);
 };
 
 export default InputFields;
-
